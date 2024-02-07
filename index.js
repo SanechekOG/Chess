1f21470444af12367b002b5d551e1922c8324aab
SanechekOG <159322559+SanechekOG@users.noreply.github.com>
2024-02-07 21:31:21 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+function openTabIndex() {
+    window.open('index.html', '_blank');
+}
+function openTabPlay() {
+    window.open('Play_chess.html', '_blank');
+}
+function openTabLearn() {
+    window.open('Learn_chess.html', '_blank');
+}
+function openTabHistory() {
+    window.open('History_of_chess.html', '_blank');
+}
+function openTabNews() {
+    window.open('News_of_chess.html', '_blank');
+}
+function openTabChat() {
+    window.open(href ='https://vk.me/join/yPl3T9MPOXxyHTgns1IXgGEfqrvBHi8ey0w=', '_blank');
+}
